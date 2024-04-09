5b36a0492552a4fd151fb8c1a4642c0b1c418446
dsridhard <73626340+dsridhard@users.noreply.github.com>
2024-04-09 08:09:56 +0530

    user registration service integrate

--- src/app/user-auth/signup/signup.component.ts
+++ src/app/user-auth/signup/signup.component.ts
@@ -3,39 +3,41 @@ import { CoursesService } from 'src/app/courses.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
   coursesData: any;
   selected: string | undefined;
   employeeId: Boolean | undefined;
-
+  selectedPersonType: string = 'self'; // Initialize with a default value
+  cugNumber: number | undefined;
+  sameNumberChecked: boolean = false;
   constructor(private courseDetail: CoursesService) {
-
     courseDetail.getCourse().subscribe((courseResponse) => {
       // console.log(data)
-      this.coursesData = courseResponse
-    })
-
-
+      this.coursesData = courseResponse;
+    });
   }
-
+  // handleCheckboxChange() {
+  //   if (this.sameNumberChecked) {
+  //     // Set CUG Number to Whatsapp Number value
+  //     this.cugNumber = this.whatsappInput.value;
+  //   }
+  // }
   selectOption(value: string): void {
     this.selected = value; // Update the variable with the selected value
-    console.log(this.selected)
-    if (this.selected == "Railways") {
-      this.employeeId = false
+    console.log(this.selected);
+    if (this.selected == 'Railways') {
+      this.employeeId = false;
       // console.log(this.employeeId)
     } else {
-      this.employeeId = true
+      this.employeeId = true;
     }
   }
 
-  selectEmpGrp(value: string): void {
-  }
-
+  selectEmpGrp(value: string): void {}
 
   signupHandler(data: any) {
-    console.log(data)
+    console.log(data);
   }
 }
