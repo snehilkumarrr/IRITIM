ac9a726adcaf8b32afbf908f750f045f40e74595
test <snehilk10@gmail.com>
2024-07-02 12:07:18 +0530

    small changes

--- src/app/Shared/constants.ts
+++ src/app/Shared/constants.ts
@@ -1,6 +1,6 @@
 
-const parsedUrl = new URL(window.location.origin);
-export const BASE_URL                      = parsedUrl +'IRITM/';
-console.log(BASE_URL);
-//export const BASE_URL                      = 'https://testiritm.indianrailways.gov.in/IRITM/';
+// const parsedUrl = new URL(window.location.origin);
+// export const BASE_URL                      = parsedUrl +'IRITM/';
+// console.log(BASE_URL);
+export const BASE_URL                      = 'https://iritm.indianrailways.gov.in/IRITM/';
 // export const BASE_URL                      = 'https://10.64.29.243:8080/IRITM/';
