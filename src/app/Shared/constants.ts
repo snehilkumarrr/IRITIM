e72f7e764ff9636467213fd4f880d59d6fc0cbb1
haider <ali511.haider@gmail.com>
2024-07-19 16:44:09 +0530

    dynamic data

--- src/app/Shared/constants.ts
+++ src/app/Shared/constants.ts
@@ -1,6 +1,11 @@
 
-const parsedUrl = new URL(window.location.origin);
-export const BASE_URL                      = parsedUrl +'IRITM/';
-console.log(BASE_URL);
-//export const BASE_URL                      = 'https://iritm.indianrailways.gov.in/IRITM/';
-// export const BASE_URL                      = 'https://10.64.29.243:8080/IRITM/';
+// const parsedUrl = new URL(window.location.origin);
+// export const BASE_URL                      = parsedUrl +'IRITM/';
+// console.log(BASE_URL);
+export const BASE_URL                      = 'https://iritm.indianrailways.gov.in/IRITM/';
+// export const BASE_URL                      = 'https://10.64.29.243:8080/';
+// export const BASE_URL                      = 'http://172.16.14.78:8080/';
+
+
+
+ //http://10.64.29.243:8080/IRITM/Dashboard/getcontent/0,1
